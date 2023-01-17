60ce3601edc9889d04b66bf4e808f1a5e90392ba
himanshumernstack <himanshu.bluebird@gmail.com>
2023-01-17 11:54:15 +0530

    Prepare for deployment

---
+++ build/precache-manifest.0c7e1c238195cc06127cb77320f3469a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7de1ee17fb198da042863264bdd2905",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59e4eaf7405b4079e651",
+    "url": "/static/css/main.9450bc60.chunk.css"
+  },
+  {
+    "revision": "7b14f9f6940f9acca2e5",
+    "url": "/static/js/2.4b986f4e.chunk.js"
+  },
+  {
+    "revision": "59e4eaf7405b4079e651",
+    "url": "/static/js/main.b5309b48.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
